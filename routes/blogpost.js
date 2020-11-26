87959c1efa18702cae22d5b9fffceb7909879684
James Nguyen <viethuy25@gmail.com>
2020-11-26 10:36:58 -0500

    proper structure MVC

---
+++ routes/blogpost.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const {BlogPostSchemaJoi} = require('../models/blogPostJoi');
+
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const BlogPost = require('../models/blogpost');
+const e = require('express');
+
+const validateBlogPost = (req, res, next) => {
+    const { error } = BlogPostSchemaJoi.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+//view all posts
+router.get('/', catchAsync(async(req,res)=>{
+    const posts = await BlogPost.find({});
+    res.render('posts/posts', { posts });
+}));
+
+//new post page
+router.get('/create',(req,res) => {
+    res.render('posts/create');
+});
+
+//public new post
+router.post('/', validateBlogPost, catchAsync(async(req,res ,next)=>{
+    const post = new BlogPost(req.body.post);
+    await post.save();
+    res.redirect('/Posts')
+}));
+
+//view specific post page
+router.get('/:id', catchAsync(async(req,res, next)=>{
+    const post = await BlogPost.findById(req.params.id);
+    res.render('posts/show', { post });
+}));
+
+//edit post
+router.get('/:id/edit', catchAsync(async(req,res, next)=>{
+    const post = await BlogPost.findById(req.params.id);
+    res.render('posts/edit', { post });
+}));
+
+router.put('/:id', validateBlogPost, catchAsync(async(req,res, next) => {
+    const { id } = req.params;
+    const post = await BlogPost.findByIdAndUpdate(id, { ...req.body.post });
+    console.log(post._id);
+    res.redirect('/Posts')
+}));
+
+//delete post
+router.get('/:id/delete', catchAsync(async(req,res, next)=>{
+    const post = await BlogPost.findById(req.params.id);
+    res.render('posts/delete', { post });
+}));
+
+router.delete('/:id', catchAsync(async(req,res, next) => {
+    const { id } = req.params;
+    await BlogPost.findByIdAndDelete(id);
+    res.redirect('/Posts');
+}));
+
+module.exports= router;
