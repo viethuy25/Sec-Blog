88e0f3f65c92dece6086fa1ad29a118374244a20
James Nguyen <viethuy25@gmail.com>
2020-11-26 11:45:11 -0500

    working on log in and authentication

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
+const flash = require('connect-flash');
+
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const e = require('express');
@@ -31,6 +33,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+const sessionConfig = {
+    secret: 'thisshouldbebettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 *24 *7,
+        age: 1000 * 60 * 60 *24 *7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req,res, next) => {
+    res.locals.success = req.flash('success');
+    next();
+})
 
 app.use('/Posts', blogpost)
 
