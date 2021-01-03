ed7e86f4117b17509630886da70598b58b75db41
James Nguyen <viethuy25@gmail.com>
2021-01-03 12:45:33 -0500

    configure to switch to heroku

--- app.js
+++ app.js
@@ -136,6 +136,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err});
 });
 
-app.listen(443, () => {
-    console.log('Serving on port 443');
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log('Serving on port ', port);
 });
